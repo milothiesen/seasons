427d52bc70da566e07e8df98737494e354c58ddd
Thiesen <mailethiesen@gmail.com>
2021-02-09 09:29:28 -0500

    fixed footer

--- src/Footer.js
+++ src/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => (
         <p>
             This site was made based on Stephen Grider's course Modern
             React/Redux [2020] on Udemy.com. This site reads a users location
-            and determines whether they are in the northernn or southern
+            and determines whether they are in the northern or southern
             hemisphere and then returns an appropriate art object for that
             season.
         </p>
